f66a8b7452751b08d92ef2568af05c8dc2e8d5c1
juli7julia7 <106538206+juli7julia7@users.noreply.github.com>
2023-02-07 22:37:34 +0200

    new

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$('.question__item > div').on('click', function () {
+   $(this).children('.question__img').toggleClass('question__img--min-icon');
+   $(this).next('.question__info').slideToggle(300)
+   
+  
+})
