45fd430029dcf0e1325ef99610985661879d22d1
juli7julia7 <juli7julia7@gmail.com>
2024-05-20 11:07:49 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
 $('.question__item > div').on('click', function () {
    $(this).children('.question__img').toggleClass('question__img--min-icon');
    $(this).next('.question__info').slideToggle(300)
-   
-  
-})
+});
